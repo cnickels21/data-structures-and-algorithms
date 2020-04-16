2c2dba7d83d0eb3d404d1ae715cd67c1db92e730
cnickels21 <cnickels_21@yahoo.com>
2020-04-15 20:54:57 -0500

    7 passed

--- code-challenges/includes-etc/challenges-13.test.js
+++ code-challenges/includes-etc/challenges-13.test.js
@@ -99,7 +99,11 @@ Write a function named findEvery that takes in an array of strings, along with a
 ------------------------------------------------------------------------------------------------ */
 
 const findEvery = (arr, target) => {
-  // Solution code here...
+  return arr.every(item => {
+    if (item.includes(target)) {
+      return true;
+    }
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
